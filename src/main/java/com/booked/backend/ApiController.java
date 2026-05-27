f52310514515f78355eaacfa4d156f8eab8621b1
LucasCoufal <lucas.coufal@edu.unirio.br>
2026-05-27 11:06:51 -0300

    Adiciona o projeto base

---
+++ src/main/java/com/booked/backend/ApiController.java
@@ -0,0 +1,10 @@
+package com.booked.backend;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    // Aqui dá pra fazer transferência de dados entre o java e javascript
+
+}
