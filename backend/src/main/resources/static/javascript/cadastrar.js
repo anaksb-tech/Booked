2976820e00139c028eb91f01be78a3ee8f6e6348
LucasCoufal <lucas.coufal@edu.unirio.br>
2026-06-01 22:36:20 -0300

    Corrige um erro no cadastro

--- backend/src/main/resources/static/javascript/cadastrar.js
+++ backend/src/main/resources/static/javascript/cadastrar.js
@@ -8,28 +8,32 @@ document.getElementById("confirmarCadastro").onclick = async function() {
 
     // Valida os dados do formulário
     if(nome === "") {
-
+        window.alert("Nome é um campo obrigatório");
     } else if(genero === "") {
-
+        window.alert("Gênero é um campo obrigatório");
     } else if(email === "") {
-
+        window.alert("Email é um campo obrigatório");
     } else if(senha === "") {
+        window.alert("Senha é um campo obrigatório");
+    } else {
+
+        // Cria o bloco de dados do usuário
+        let usuario = {
+            nome: nome,
+            genero: genero,
+            email: email,
+            senha: senha
+        };
+
+        // Envia o usuário pro backend
+        await fetch("http://localhost:8080/usuario/cadastrar-usuario", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(usuario)
+        });
 
     }
 
-    // Cria o bloco de dados do usuário
-    let usuario = {
-        nome: nome,
-        genero: genero,
-        email: email,
-        senha: senha
-    };
 
-    // Envia o usuário pro backend
-    await fetch("http://localhost:8080/usuario/cadastrar-usuario", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(usuario)
-    });
 
 }
