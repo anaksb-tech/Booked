8fae3e0124432fdb2fdf48ab5798eb26ee9fdfba
LOt-0 <elli0ttomail@gmail.com>
2026-06-13 22:09:42 -0300

    Delete src directory

--- src/main/java/com/booked/backend/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.booked.backend.repository;
-
-import com.booked.backend.entity.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Integer> {
-}
