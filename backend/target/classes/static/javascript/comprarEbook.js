76a5196f92b63e62556ca64b0589891e057d5a7c
LOt-0 <elli0ttomail@gmail.com>
2026-06-13 23:13:27 -0300

    Add files via upload

---
+++ backend/target/classes/static/javascript/comprarEbook.js
@@ -0,0 +1,43 @@
+// Botão Comprar
+document.getElementById("botaoComprar").onclick = async function() {
+
+    // Pegar entradas dos inputs
+    const numeroCartao = document.getElementById("inputNumeroCartao").value;
+    const nomeCartao = document.getElementById("inputNomeCartao").value;
+    const codigoCodigoSeguranca = document.getElementById("inputCodigoSeguranca").value;
+
+    // Buscar usuário comprador
+    const idUsuario = localStorage.getItem("idUsuario");
+    let resposta = await fetch(`http://localhost:8080/usuario/buscar-usuario/${idUsuario}`);
+    const usuario = await resposta.json();
+
+    // Pegar o id do eBook do url da página
+    const parametro = new URLSearchParams(window.location.search);
+    const id = parametro.get("id");
+
+    // Buscar eBook
+    resposta = await fetch(`http://localhost:8080/ebook/${id}`);
+    const ebook = await resposta.json();
+
+    // Criar bloco de dados da compra
+    const compra = {
+        usuario: usuario,
+        ebook: ebook,
+        valor: ebook.preco,
+    }
+
+    // Registrar no banco de dados
+    resposta = await fetch("https://localhost:8080/ebook/publicar", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(compra)
+    });
+
+    if(resposta.ok) {
+        window.alert("eBook comprado com sucesso");
+        window.location.href = "/html/index.html";
+    } else {
+        window.alert("Algo deu errado");
+    }
+
+}
